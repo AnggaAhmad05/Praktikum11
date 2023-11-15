8e7b1fd3d052ab1e3500a78e59cb783860b86e3a
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-11-15 14:40:47 +0700

    kota

--- NestedLoop05.java
+++ NestedLoop05.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class NestedLoop05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+         
         double rata = 0;
-        double[][] suhu = new double[2][4];
+        System.out.println(" Masukkan Jumlah Kota: ");
+         int Jumlahkota = sc.nextInt();
+        double[][] suhu = new double[Jumlahkota][4];
 
         for (int i = 0; i < suhu.length; i++) {
             System.out.println("KOTA KE- " + (i+1));
@@ -28,18 +30,7 @@ public class NestedLoop05 {
             System.out.println();
             rata = total / suhu[i].length;
             System.out.println("RATA RATA SUHU ADALAH "+rata);
+
         }
-        // for (int i = 0; i < suhu.length; i++) {
-        //     System.out.print("Rata-rata suhu: " + (i+1));
-        //     double total = 0;
-
-        //     for (int j = 0; j < suhu[i].length; j++) {
-        //         total += suhu[i][j];
-        //     }
-        //     rata =(double)total / suhu[i].length;
-        //     System.out.println(rata);
-
-        //     System.out.println();
-        // }
     }
 }
