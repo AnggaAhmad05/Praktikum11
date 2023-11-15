1c68f4abadea7c36dfbe208760112ccc90e2013f
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-11-15 14:24:58 +0700

    rata-rata

--- NestedLoop05.java
+++ NestedLoop05.java
@@ -3,6 +3,7 @@ public class NestedLoop05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        double rata = 0;
         double[][] suhu = new double[2][4];
 
         for (int i = 0; i < suhu.length; i++) {
@@ -16,13 +17,29 @@ public class NestedLoop05 {
     
         }
         for (int i = 0; i < suhu.length; i++) {
-            System.out.print("KOTA KE- " + (i+1) + ": ");
+            System.out.print("KOTA KE-: " + (i+1) + ": ");
+            double total = 0;
+        
+        for (double Suhu  : suhu[i]) {
+            System.out.print(Suhu + " ");
+            total += Suhu;
             
-            for (int j = 0; j < suhu[i].length; j++) {
-                System.out.print(suhu[i][j] + " ");
-
-            }
+        }    
             System.out.println();
+            rata = total / suhu[i].length;
+            System.out.println("RATA RATA SUHU ADALAH "+rata);
         }
+        // for (int i = 0; i < suhu.length; i++) {
+        //     System.out.print("Rata-rata suhu: " + (i+1));
+        //     double total = 0;
+
+        //     for (int j = 0; j < suhu[i].length; j++) {
+        //         total += suhu[i][j];
+        //     }
+        //     rata =(double)total / suhu[i].length;
+        //     System.out.println(rata);
+
+        //     System.out.println();
+        // }
     }
 }
