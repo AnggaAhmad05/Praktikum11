1385554b5625e862b463c37fdc89116003855f93
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-11-15 15:42:19 +0700

    Angkapersegi

--- Angka05.java
+++ Angka05.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
 public class Angka05 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Masukan Angka : ");
+        int n = sc.nextInt();
+
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+                
+            }
+            for (int j = 0; j < i; j++) {
+                int angka = j + 1;
+                System.out.print(angka);
+                
+            }
+            System.out.println();
+        }
+        
+    }
     
 }
     
-}
+
---
+++ AngkaPersegi05.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class AngkaPersegi05 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Masukan jumlah angka :"  );
+    int n =sc.nextInt();
+
+    if(n > 2 && n % 2 !=0) {
+    for (int i = 1; i <= n; i++) {
+
+        for (int j = 1; j <= n; j++) {
+            if (i == 1 || i == n  || j == 1 || j == n) {
+                System.out.print(n + " ");
+
+            }else {
+                System.out.print("  ");    
+            
+            }
+        
+            }
+            System.out.println();
+        }
+        } else{
+            System.out.println("Jumlah angka tidak valid");
+    
+    
+    }
+}
+}
