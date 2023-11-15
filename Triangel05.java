8ef14c386775eb69b03dfe5cafdf03a025b7e3e8
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-11-15 11:36:11 +0700

    Triangel

--- Triangel05.java
+++ Triangel05.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
-public class Square05 {
+public class Triangel05 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukan jumlah *: ");
         int n = sc.nextInt();
-      
-         for (int i = 1; i <=n; i++) {
-            for (int j = 1; j <= n; j++) {
+
+        int i = 0;
+
+        while (i < n) {
+            int j = 0;
+
+            while (j <= i) {
                 System.out.print("*");
+                j++;
                 
             }
             System.out.println();
-            
-         }       
-
-    }     
+            i++;
+        }
+    
+    }
+    
 }
-    
-    
-
