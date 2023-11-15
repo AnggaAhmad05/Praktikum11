c303abd0ad217d6c0ead4949cd07bc3fc8b7f288
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-11-15 11:14:38 +0700

    Square

--- Star05.java
+++ Star05.java
@@ -6,7 +6,7 @@ public class Star05 {
         System.out.print("Masukan jumlah *: ");
         int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i--) {
             System.out.print("*");
         }
     
